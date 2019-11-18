4d5ccfc550efb8d71ddaa12a2903ce9af392da12
Joshua Porter <porterjoshua1998@gmail.com>
2019-11-18 16:38:15 -0600

    commit

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -10,9 +10,34 @@ namespace Movies.Pages
     public class IndexModel : PageModel
     {
 
+        MovieDatabase movieDatabase = new MovieDatabase();
+
+        public List<Movie> Movies;
+
         public void OnGet()
         {
+            Movies = movieDatabase.All;
+        }
 
+        public void OnPost(string search, List<string> rating)
+        {
+            if (search != null && rating.Count != 0)
+            {
+                Movies = movieDatabase.SearchAndFilter(search, rating);
+            }
+            else if (search == null && rating.Count != 0)
+            {
+                Movies = movieDatabase.Search(search);
+            }
+            else if (search != null && rating.Count == 0)
+            {
+                Movies = movieDatabase.Filter(rating);
+            }
+            else
+            {
+                Movies = movieDatabase.All;
+            }
+            
         }
     }
 }
